7c709d678fee366fb8883eccad3fdb2a8db6ab02
Vitalik <you@example.com>
2024-06-13 14:15:52 +0300

    Initial implementation of User class (fetch, save, props change). Added json-server to persist data

---
+++ web/src/models/User.ts
@@ -0,0 +1,66 @@
+import axios, {AxiosResponse} from 'axios';
+
+export interface UserProps{
+  //optional properties
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+type Callback = ()=>void;
+
+export class User{
+  events:{[key: string]:Callback[]} ={};
+
+  constructor(private data: UserProps){
+    
+  }
+  
+  get(propName: string): string | number{
+    return this.data[propName];
+  }
+
+  set(update: UserProps):void{
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void{
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+
+    console.log(this.events);
+  }
+
+  trigger(eventName:string):void{
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0){
+      return;
+    } 
+
+    handlers.forEach(callback=>{
+      callback();
+    });
+  }
+
+  fetch(): void{
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse):void=>{
+          this.set(response.data);
+      });
+  }
+
+  save():void{
+    const id = this.get('id') 
+    if (id){
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+      console.log(`Updated existing user ${id}`);
+      
+    }
+    else{
+      axios.post('http://localhost:3000/users', this.data);
+      console.log('Created new user');
+    }
+  }
+}
