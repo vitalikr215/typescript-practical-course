ed81b6526d98dbf25f4b70df08d2361d00c12348
vitalikr <you@example.com>
2024-07-04 15:12:19 +0300

    Standalone event handlers for different events of html elements

---
+++ react-ts/react-ts/src/events/EventComponent.tsx
@@ -0,0 +1,23 @@
+import { ChangeEventHandler } from "react";
+
+const EventComponent: React.FC = ()=>{
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    console.log(event);
+  }
+
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>)=>{
+    console.log(event);
+  };
+
+  return(
+    <div>
+      <input onChange={onChange}/>
+      <div draggable onDragStart={onDragStart} style={{borderStyle:'dashed', borderColor:'gray'}}>
+          Drag me !
+      </div>
+    </div>
+  )
+};
+
+export default EventComponent;
